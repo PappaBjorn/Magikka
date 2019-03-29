87f32283d7264f0c6124aefe2f218c709edef0aa
JutalP <victor.jespersen@hotmail.se>
2019-03-29 18:28:52 +0100

    AI Behaviour Script and PathPatrol Script
    The AI behaviour script and the pathpatrol script aswell as the NavMesh in the sscene

---
+++ Assets/Scripts/AI/PathPatrol.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class PathPatrol : MonoBehaviour
+{
+    public Vector3[] PathGoals = new Vector3[5];
+    [SerializeField] Vector3 CurrentPatrolPoint;
+
+    private NavMeshAgent nav;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void MoveToNextPatrolPoint()
+    {
+        if (CurrentPatrolPoint == null || CurrentPatrolPoint == PathGoals[5])
+            CurrentPatrolPoint = PathGoals[0];
+
+        else if (CurrentPatrolPoint == PathGoals[0])
+            CurrentPatrolPoint = PathGoals[1];
+
+        else if (CurrentPatrolPoint == PathGoals[1])
+            CurrentPatrolPoint = PathGoals[2];
+
+        else if (CurrentPatrolPoint == PathGoals[2])
+            CurrentPatrolPoint = PathGoals[3];
+
+        else if (CurrentPatrolPoint == PathGoals[3])
+            CurrentPatrolPoint = PathGoals[4];
+
+        else if (CurrentPatrolPoint == PathGoals[4])
+            CurrentPatrolPoint = PathGoals[5];
+
+        nav.SetDestination(CurrentPatrolPoint);
+    }
+
+}
