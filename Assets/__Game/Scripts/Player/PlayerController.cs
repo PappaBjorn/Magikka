4df4f8e8d137e92c9605e3eb50d6fb6e694f24bd
Fredrik Holmgren <fredrik.ambjorn.holmgren@gmail.com>
2019-04-03 15:36:03 +0200

    TestLevel + Controller setup
    Like description

---
+++ Assets/__Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Assert.IsNotNull(agent, "<color=#FF0000><b>Error</b></color>Error: Failed to locate NavMeshAgent for Player.");
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis(Horizontal))
+        {
+
+        }
+    }
+}
