0319eb08c9c688284cb035fe27d68102b4e169d7
SmailZee <alan.smail@futuregames.nu>
2019-04-04 09:33:35 +0200

    AIRanged
    Added some prefabs and code to the AIRanged

--- Assets/__Test/Scripts/AIRanged.cs
+++ Assets/__Test/Scripts/AIRanged.cs
@@ -3,17 +3,26 @@ using UnityEngine.AI;
 
 public class AIRanged : MonoBehaviour
 {
-
+    [Header("Stats")]
     public float lookRadius = 10f;
+    public float inRange = 5f;
+    public float speed;
+    public float stopDistance;
+    public float nearDistance;
+    public float timeBtwShoots;
 
+    [Header("References")]
     public Transform target;
-    NavMeshAgent agent;
+    public NavMeshAgent agent;
+    public GameObject shoots;
+
+    private Transform bulletSpawner;
 
     void Start()
     {
         target = GameManager.Instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
-        
+        bulletSpawner = transform.Find("BulletSpawn");
     }
 
     private void Update()
@@ -21,14 +30,26 @@ public class AIRanged : MonoBehaviour
 
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if (distance <= lookRadius)
+        if (distance > lookRadius)
         {
 
             agent.SetDestination(target.position);
 
         }
+        float distanceBetween = Vector3.Distance(target.position, transform.position);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            GameObject go = Instantiate(shoots, bulletSpawner.position, Quaternion.identity) as GameObject;
+            Rigidbody rigiBod = go.GetComponent<Rigidbody>();
+            rigiBod.AddForce(transform.forward * 5000f);
+
+        }
 
     }
+
+
 
     private void OnDrawGizmosSelected()
     {
